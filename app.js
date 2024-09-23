479da85b18474a6615fbed393664f09d1b404012
jgranger3 <jgranger3@ivytech.edu>
2024-09-23 18:34:03 -0400

    Updating my file

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const morgan = require('morgan');
+
+//express app
+const app = express();
+
+//register view engine
+app.set('view engine', 'ejs');
+
+// listen for request
+app.listen(3000);
+
+//middleware & static files
+app.use(express.static('public'));
+app.use(morgan('dev'));
+
+app.get('/', (req, res) =>{
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+
+    ];
+    //res.send('<p>home page</p>');
+    res.render('index', {title:'Home', blogs});
+});
+
+app.get('/about', (req, res) =>{
+    //res.send('<p>about page</p>');
+    res.render('about', {title:'About'});
+});
+
+app.get('/blogs/create', (req, res) =>{
+    res.render('create', {title:'Create a new blog'});
+})
+
+// 404 page
+app.use((req, res) =>{
+    res.status(404).render('404', {title:'404'});
+});
