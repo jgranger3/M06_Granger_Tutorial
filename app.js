e7c282deeff8aaafe053f065e4c8cd090004b6bf
jgranger3 <jgranger3@ivytech.edu>
2024-09-23 18:56:46 -0400

    adding mongoose, and creating the connection.

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 //express app
 const app = express();
 
+const dbURI = 'mongodb+srv://jgranger3:SDEV255@nodes-tutorial.w4fan.mongodb.net/';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+
 //register view engine
 app.set('view engine', 'ejs');
 
-// listen for request
-app.listen(3000);
 
 //middleware & static files
 app.use(express.static('public'));
