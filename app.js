2b70e0802f5f740c9418c3276e05bb0c4f457589
jgranger3 <jgranger3@ivytech.edu>
2024-09-24 14:00:33 -0400

    adding a details folder, doing posting, updating the css page, and updating the app.js page.

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set('view engine', 'ejs');
 
 //middleware & static files
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true}));
 app.use(morgan('dev'));
 
 //routes
@@ -33,11 +34,34 @@ app.get('/blogs', (req, res) => {
     Blog.find().sort({createdAt: -1})
         .then((result) => {
             res.render('index', {title: 'All Blogs', blogs: result})
-
         })
         .catch((err) => {
             console.log(err);
         })
+});
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+
+})
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then(result => {
+            res.render('details', {blog: result, title: 'Blog Details'});
+        })
+        .catch(err => {
+            console.log(err);
+        });
 })
 
 app.get('/blogs/create', (req, res) =>{
